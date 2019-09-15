21c85874ba15451d7ecb4d969fa01998d52dd3c1
Shahrad Kashani <kashani.sh@tiddev.com>
2019-09-15 12:50:05 +0430

    complete graphQl chapter 8 ebook

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { GraphQlUrl } from "../data/Url";
-import { OrdersConnector } from "./OrdersConnector"
+import { OrdersConnector } from "./OrdersConnector";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToggleLink } from "../components/ToggleLink";
+import { ConnectedProducts } from "./ProductsConnector";
+import { ProductEditor } from "./ProductEditor";
+import { ProductCreator } from "./ProductCreator";
 const graphQlClient = new ApolloClient(
     {
         uri: GraphQlUrl
@@ -19,7 +24,19 @@ export class Admin extends Component {
                 </div>
                 <div className="row">
                     <div className="col p-2">
-                        <OrdersConnector />
+                        <div className="col-3 p-2">
+                            <ToggleLink to="/admin/orders">Orders</ToggleLink>
+                            <ToggleLink to="/admin/products">Products</ToggleLink>
+                        </div>
+                        <div className="col-9 p-2">
+                            <Switch>
+                                <Route path="/admin/orders" component={ OrdersConnector } />
+                                <Route path="/admin/products/create" component={ ProductCreator} />
+                                <Route path="/admin/products/:id" component={ ProductEditor} />
+                                <Route path="/admin/products"  component={ ConnectedProducts } />
+                                <Redirect to="/admin/orders" />
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/admin/ProductsTable.js
+++ src/admin/ProductsTable.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { PaginationControls } from "../PaginationControls";
+import { PaginationControls } from "../components/PaginationControls";
 import { ProductsRow } from "./ProductsRow";
 export class ProductsTable extends Component {
     render = () =>
